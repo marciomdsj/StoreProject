ccf2ecece9b441bb5fe5a7b1c559dccd49c4cb4f
marcio <marciomartins3222@hotmail.com>
2025-01-10 22:41:59 -0300

    Adding cart price

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const products = {
    tv: [
-    {name: 'Tv Samsung 40', imageUrl: './images/samsung.jpeg', price: 'R$1800,00' },
-    {name: 'Tv Philco 32', imageUrl: './images/philco.jpeg', price: 'R$1200,00'}
+    {name: 'Tv Samsung 40', imageUrl: './images/samsung.jpeg', price: '1800,00' },
+    {name: 'Tv Philco 32', imageUrl: './images/philco.jpeg', price: '1200,00'}
    ],
    cellphone: ['iPhone', 'Samsung'],
    notebook: ['Acer', 'Lenovo'],
@@ -11,6 +11,7 @@ function showProducts(){
     const categorySelect = document.getElementById('categorySelect');
     const productList = document.getElementById('productList');
     const selectedCategory = categorySelect.value;
+    let sum = 0;
 
     if(selectedCategory){
         productList.innerHTML ='';
@@ -26,32 +27,51 @@ function showProducts(){
 
             productName.textContent = product.name;
 
+
             productItem.appendChild(productImage);
             productItem.appendChild(productName);
             productList.appendChild(productItem);
+           
         });
+
         productList.style.display = 'block';
     }else{
         productList.style.display = 'none';
     }
 };
 
+let sum = 0;
+
 function addToCart(product){
     const cartItems = document.getElementById('cartItems');
     const cartItem = document.createElement('li');
     const itemInfo = document.createElement('span');
     const button = document.createElement('button');
-    itemInfo.textContent = `${product.name} - ${product.price}`;
+    const price = parseFloat(product.price.replace(',', '.'));
+    sum += price;
+
+    itemInfo.textContent = `R$${product.name} - R$${product.price}`;
     button.textContent = `Remove from cart`;
     button.addEventListener('click', () => {
-        removeFromCart(cartItem);
-    })
+        removeFromCart(cartItem, price);
+    });
+
+    
+
     cartItems.appendChild(cartItem);
     cartItem.appendChild(button);
     cartItem.appendChild(itemInfo);
+
+    sumTotal();
 }
 
-function removeFromCart(item){
+function removeFromCart(item, price){
     item.remove();
+    sum -= price;
+    sumTotal();
+}
 
+function sumTotal(){
+    const totSum = document.getElementById('sum');
+    totSum.textContent = `Total: R$${sum.toFixed(2)}`;
 }
